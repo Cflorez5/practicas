1bb2bfd365416ad21540e7e1d326c9fe6621ec6c
CrissFC <cflorez@futurespace.es>
2022-11-04 13:16:15 +0100

    Laboratorio Módulo 8. Read me incluye Code Sandbox

---
+++ Módulo8-Imports/src/index.js
@@ -0,0 +1,8 @@
+import * as clientListBusiness from "./client-list-business";
+
+// index.js
+
+
+window.onload = function() {
+    clientListBusiness.printClientsAccounts();
+};
--- README.md
+++
@@ -1,3 +0,0 @@
-# practicas
-
-Prácticas del Bootcamp Continuo de Lemon Code (2022)
